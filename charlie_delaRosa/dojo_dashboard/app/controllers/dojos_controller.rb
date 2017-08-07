606f6a7b3861151fd85cb152cd42083e3c9f4055
Your Name <Charlie@Charlies-MacBook-Pro.local>
2017-08-07 15:06:57 -0500

    updated project to assignment Dojo Dashboard II

--- charlie_delaRosa/dojo_dashboard/app/controllers/dojos_controller.rb
+++ charlie_delaRosa/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -1,5 +1,20 @@
 class DojosController < ApplicationController
   def index
-    render 'index'
+    render 'dojos/index'
+  end
+
+  def new
+
+  end
+
+  def create
+    #Dojo.create( branch: params[:branches][:branch], street: params[:branches][:street], city: params[:branches][:city], state: params[:branches][:state])
+    Dojo.create(dojo_params)
+    redirect_to "/"
+  end
+
+  private
+  def dojo_params
+    params.require(:branches).permit(:branch, :street, :city, :state)
   end
 end
--- charlie_delaRosa/dojo_dashboard/config/routes.rb
+++ charlie_delaRosa/dojo_dashboard/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'dojos/index'
-  
+  get 'dojos/index' => 'dojos#index'
+  get '/dojos/new' => 'dojos#new'
+  post '/dojos/new' => "dojos#create"
+  get '/index' => 'dojos#index'
+  get '/' => 'dojos#index'
 end
