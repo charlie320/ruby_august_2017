4af1ab4800a3c15eb4286d950393c6420ebc7982
Your Name <Charlie@Charlies-MBP.home>
2017-08-08 23:59:29 -0500

    student_dashboard assignment

--- charlie_delaRosa/student_dashboard/app/models/dojo.rb
+++ charlie_delaRosa/student_dashboard/app/models/dojo.rb
@@ -1,3 +1,8 @@
 class Dojo < ActiveRecord::Base
   has_many :students
+
+  validates :branch, :street, :city, :state, presence: true
+  validates :branch, :city, length: { in: 2..25}
+  validates :street, length: { in: 5..25}
+  validates :state, length: { is: 2}
 end
--- charlie_delaRosa/student_dashboard/app/models/student.rb
+++ charlie_delaRosa/student_dashboard/app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ActiveRecord::Base
   belongs_to :dojo
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, presence: true, length: { in: 2..50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX}
 end
