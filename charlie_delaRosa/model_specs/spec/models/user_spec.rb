c2b18827b6b3054cd37f57059fd1e8a1727fa01f
Your Name <Charlie@Charlies-MacBook-Pro.local>
2017-08-11 10:56:50 -0500

    models_spec assignment

---
+++ charlie_delaRosa/model_specs/spec/models/user_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+RSpec.describe User do
+  # This is our control
+  context "With valid attributes" do
+    it "should save" do
+      user = User.new(
+        first_name: 'shane',
+        last_name: 'chang',
+        email: 'schang@codingdojo.com'
+      )
+      expect(user).to be_valid
+    end
+  end
+
+  context "With invalid attributes" do
+    it "should not save if first_name field is blank" do
+      user = User.new(
+            first_name: '',
+            last_name: 'chang',
+            email: 'schang@codingdojo.com'
+        )
+      expect(user).to be_invalid
+    end
+
+    it "should not save if last_name field is blank" do
+      user = User.new(
+            first_name: 'John',
+            last_name: '',
+            email: 'schang@codingdojo.com'
+        )
+      expect(user).to be_invalid
+    end
+    it "should not save if email already exists" do
+      user1 = User.create(
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john@codingdojo.com'
+        )
+      user2 = User.new(
+            first_name: 'Jane',
+            last_name: 'Doe',
+            email: 'john@codingdojo.com'
+        )
+      expect(user2).to be_invalid
+    end
+    it "should not save if invalid email format" do
+      user = User.new(
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john#codingdojo.com'
+        )
+      user.save
+      expect(user).to be_invalid
+    end
+  end
+end
