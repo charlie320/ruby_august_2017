4916f26ccbae272dd07f31395c1eb5a57f5d570f
Your Name <Charlie@Charlies-MacBook-Pro.local>
2017-08-11 16:08:27 -0500

    repeated rspec_for_rails (model_specs)

--- charlie_delaRosa/model_specs/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
-  validates :first_name, :last_name, presence: true
-  #validates :email, uniqueness: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX}
-end
---
+++ charlie_delaRosa/rspec_for_rails/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  validates :first_name, :last_name, presence: true
+  ##following line has same effect as first line
+  # validates :first_name, :presence => true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX}
+
+end
